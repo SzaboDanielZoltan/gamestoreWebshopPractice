ae780a106b447ff7c6538b6deed8e65092d41bab
Szabo Daniel Zoltan <szabo.daniel.zoltan@gmail.com>
2019-08-18 23:34:01 +0200

    Admin product ready with CRUD

--- src/app/page/changeproduct/changeproduct.component.ts
+++ src/app/page/changeproduct/changeproduct.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product';
+import { OrderService } from 'src/app/service/order.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-changeproduct',
   templateUrl: './changeproduct.component.html',
   styleUrls: ['./changeproduct.component.css']
 })
-export class ChangeproductComponent implements OnInit {
+export class ChangeproductComponent {
+  urlForServer = '';
+  oneOfTheDatas = '';
+  product: Product;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private os: OrderService,
+    private router: Router
+  ) {
+    this.urlForServer = this.router.url.split('/')[2];
+    if (this.router.url.split('/')[3] === '0') {
+      this.product = new Product();
+    } else {
+      this.oneOfTheDatas = this.router.url.split('/')[3];
+      this.os.getAll(this.urlForServer, this.oneOfTheDatas).forEach(
+        product => this.product = product
+      )
+    }
   }
 
+  onSubmit(event: Event): void {
+    event.preventDefault();
+    if (this.router.url.split('/')[3] === '0') {
+      this.os.add(this.urlForServer, this.product).forEach(
+        data => this.router.navigateByUrl("/admin/products")
+      );
+    } else {
+      this.os.edit(this.product).forEach(
+        data => this.router.navigateByUrl("/admin/products")
+      );
+    }
+  }
 }
